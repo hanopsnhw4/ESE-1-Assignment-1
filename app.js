d9e965e568ad6d09df6ca807edbadb6916ee4430
hanopsnhw4 <117444375+hanopsnhw4@users.noreply.github.com>
2025-04-09 10:53:46 +0000

    Render Dependency

--- app.js
+++ app.js
@@ -234,3 +234,9 @@ app.post('/logout', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
